4b8fadcbbd9ab16573bc3498b4fd3983c0517d1b
Vibhabakkam <123794453+Vibhabakkam@users.noreply.github.com>
2026-02-25 17:14:10 +0530

    Added add shape and delete shape components in app.tsx file

--- shape-composer-tool/src/App.tsx
+++ shape-composer-tool/src/App.tsx
@@ -1,9 +1,28 @@
-function App() {
+import { useState } from 'react'
+import type { ShapeLayer } from './interfaces'
+import ControlPanel from './components/ControlPanel'
+import LayersPanel from './components/LayersPanel'
+
+const App = () => {
+  const [layers, setLayers] = useState<ShapeLayer[]>([])
+
+  const handleAddShape = (newShape: ShapeLayer) => {
+    setLayers([newShape, ...layers])
+  }
+
+  const handleDeleteShape = (id: string) => {
+    setLayers(layers.filter(layer => layer.id !== id))
+  }
 
   return (
-    <>
-     <div>Hello Canvas App</div>
-    </>
+    <div>
+      <h1>Shape Composer Tool</h1>
+      <div>
+        <ControlPanel onAddShape={handleAddShape} />
+        <div>Canvas will go here</div>
+        <LayersPanel layers={layers} onDeleteShape={handleDeleteShape} />
+      </div>
+    </div>
   )
 }
 
